3c79f659ca112b9ca1831ca12b0ba1aaf8c7e153
Mahima-Dewangan <mahimadewangan11.com>
2025-09-15 23:21:10 +0530

    getter and setter functions

--- oops.cpp
+++ oops.cpp
@@ -4,17 +4,30 @@ using namespace std;
 class teacher 
 {
     // properties or attributes
+    private:
+    double salary;
+
     public:
     string name;
     string dept;
     string subject;
-    double salary;
-
+    
     // methods : functions inside the class
     // memeber function : as the functions here are the members of this class
     void changeDept(string newDept) 
     {
         dept = newDept;
+    }
+
+    // setter function : basically used to set the value of the private variable
+    void setSalary(double s )
+    {
+        salary = s;
+    }
+
+    // getter : basically used to get the value of the private variable 
+    double getSalary(){
+        return salary;
     }
 
 };
@@ -28,9 +41,11 @@ int main ()
     t1.name = "Mahima";
     t1.dept = " Computer Science";
     t1.subject = "C++";
-    t1.salary = 25000;
+    t1.setSalary(25000);  // the setter function is the public method 
+
 
     cout<<t1.name<<endl;
+    cout<<t1.getSalary()<<endl;   ///the getter function is the public method using which we can access the variable salary. 
 
     return 0;
 }
